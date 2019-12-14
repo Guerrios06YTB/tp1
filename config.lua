fc85c20b25ccf5987b563a4d47c760c57b41e743
Guerrios06YTB <58877967+Guerrios06YTB@users.noreply.github.com>
2019-12-14 11:51:08 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,73 @@
+Config                            = {}
+
+Config.Teleporters = {
+	['Complexe MS-13'] = {
+		['Job'] = 'mafia',
+		['Enter'] = { 
+			['x'] = 1866.01, 
+			['y'] = 270.90, 
+			['z'] = 164.26,
+			['Information'] = '[E] Pour Entrer',
+		},
+		['Exit'] = {
+			['x'] = 487.18, 
+			['y'] = 4819.90, 
+			['z'] = -59.38, 
+			['Information'] = '[E] Pour Sortir' 
+		}
+	},
+
+	['Bunker MS-13'] = {
+		['Job'] = 'mafia',
+		['Enter'] = { 
+			['x'] = 460.75, 
+			['y'] = 4815.63, 
+			['z'] = -60.00,
+			['Information'] = '[E] Pour Descendre Dans Le Bunker' 
+		},
+
+		['Exit'] = {
+			['x'] = 896.39, 
+			['y'] = -3245.61, 
+			['z'] = -99.24, 
+			['Information'] = '[E] Pour Remonter Au Complexe' 
+		}
+	},
+	
+	['Traitement Weed'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 983.04, 
+			['y'] = -2280.94, 
+			['z'] = 29.50,
+			['Information'] = '[E] Pour Rentrer' 
+		},
+
+		['Exit'] = {
+			['x'] = 1066.30, 
+			['y'] = -3183.44, 
+			['z'] = -40.16, 
+			['Information'] = '[E] Pour Sortir' 
+		}
+	},
+
+	['Terrasse Diamond Casino'] = {
+		['Job'] = 'mafia',
+		['Enter'] = { 
+			['x'] = 969.37, 
+			['y'] = 63.38, 
+			['z'] = 111.55,
+			['Information'] = '[E] Pour Sortir Sur La Terrasse',
+		},
+		['Exit'] = {
+			['x'] = 968.07, 
+			['y'] = 63.35, 
+			['z'] = 111.55, 
+			['Information'] = '[E] Pour Rentrer' 
+		}
+	}
+
+	--Next here
+}
+
+
